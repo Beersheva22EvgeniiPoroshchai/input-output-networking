62ba275f9f1291b6202d64bde0e904ae43fc1b11
evgenii.prs <evgenii.prs@gmail.com>
2023-03-10 08:43:23 +0200

    upd copyRun (overwrite flag)

--- src/telran/io/Copy.java
+++ src/telran/io/Copy.java
@@ -2,6 +2,8 @@ package telran.io;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
@@ -23,12 +25,13 @@ abstract long copy() throws IOException;
 abstract DisplayResult getDisplayResult (long copyTime, long fileSize);
 	
 public void copyRun() throws Exception{
- 	Instant time = Instant.now();
+ 	if (!overwrite && Files.exists(Path.of(destFilePath))) {
+ 		throw new Exception (destFilePath + "can't be overwritten");
+ 	}
+	Instant time = Instant.now();
 	long size = copy();
 	DisplayResult dispRes = getDisplayResult (ChronoUnit.MILLIS.between(time, Instant.now()), size);
 	System.out.println(dispRes);
-	
-	
 	
 }
 
