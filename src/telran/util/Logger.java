9011582309cb8f6d7a352bcfad7bd7a38ecc76a5
evgenii.prs <evgenii.prs@gmail.com>
2023-03-14 16:54:45 +0200

    fixed Logger

--- src/telran/util/Logger.java
+++ src/telran/util/Logger.java
@@ -14,47 +14,53 @@ public Logger(Handler handler, String name) {
 	this.name = name;
 }
  
-	public void setLevel (Level level) {
-		this.level = level;
-	}
-	
-	public void error (String message) {
-		if (checkLevel(level, Level.ERROR)) {
-		LoggerRecord error = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(), Level.ERROR, name, message);
-		handler.publish(error);
-	}
+
+public void setLevel(Level level) {
+	this.level = level;
 }
+
+public boolean checkLevel (Level level, Level LevelbyParam) {
+	return level.compareTo(LevelbyParam) < 1;
+}
+
+
+public void error(String message) {
+	if (checkLevel(level, Level.ERROR)) {
+		LoggerRecord error = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(),
+			Level.ERROR, name, message);
+	handler.publish(error);
+	}
 	
-	public void warn (String message) {
-		if (checkLevel(level, Level.WARN)) {
-		LoggerRecord warn = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(), Level.WARN, name, message);
+}
+public void warn(String message) {
+	if (checkLevel(level, Level.WARN)) {
+		LoggerRecord warn = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(),
+				Level.WARN, name, message);
 		handler.publish(warn);
 	}
-	}
-	
-	public void info (String message) {
-		if (checkLevel(level, Level.INFO)) {
-		LoggerRecord info = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(), Level.INFO, name, message);
+}
+
+public void info(String message) {
+	if (checkLevel(level, Level.INFO)) {
+		LoggerRecord info = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(),
+				Level.INFO, name, message);
 		handler.publish(info);
 	}
 }
-	
-	public void debug (String message) {
-		if (checkLevel(level, Level.DEBUG)) {
-		LoggerRecord debug = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(), Level.DEBUG, name, message);
+
+public void debug(String message) {
+	if (checkLevel(level, Level.DEBUG)) {
+		LoggerRecord debug = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(),
+				Level.DEBUG, name, message);
 		handler.publish(debug);
 	}
 }
-	
-	public void trace (String message) {
-		LoggerRecord trace = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(), Level.TRACE, name, message);
+
+public void trace(String message) {
+	if (checkLevel(level, Level.TRACE)) {
+		LoggerRecord trace = new LoggerRecord(Instant.now(), ZoneId.systemDefault().toString(),
+				Level.TRACE, name, message);
 		handler.publish(trace);
+		}
 	}
-
-
-	public boolean checkLevel(Level level, Level LevDefault) {
-		return level.compareTo(LevDefault) < 1;
-	}
-		
-	
 }
